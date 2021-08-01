7262f456c9ee423265e1b5c581d0ea459e3da4c9
Harsh <60095636+hahaharsh7@users.noreply.github.com>
2021-08-01 20:39:05 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,27 @@
+from seleniumwire import webdriver  # Import from seleniumwire
+from bs4 import BeautifulSoup
+
+chromedriver_location = input(str('Enter the location to your chromedriver: '))
+
+driver = webdriver.Chrome(chromedriver_location)
+
+URL = 'http://the-internet.herokuapp.com/nested_frames'
+driver.get(URL) 
+
+required = ['left' , 'middle' , 'right' , 'bottom']
+links = []
+for request in driver.requests:
+    if request.response:
+        if request.url.split('_')[-1] in required:
+            links.append(request.url)
+
+ans = []            
+for i in range(len(links)):
+    driver.get(links[i])
+    data = BeautifulSoup(driver.page_source)
+    required_text = data.find('body')
+    ans.append(required_text.text)
+    
+for i in ans:
+    i = i.strip()
+    print(i)
